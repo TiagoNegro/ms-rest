806c1015875a36678b973bfc6382a37b0581d238
tnegro <tiago.negro1@hotmail.com>
2022-11-19 21:23:52 -0300

    Novas funcionalidades dadas para o servidor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express, {Request, Response, NextFunction} from "express";
-
+import usersRoute from "./routes/users.routes";
 const app = express();
 
+/* Configurações da aplicação */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* Configurações de rotas */
+app.use(usersRoute);
 app.get("/status", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({foo: "bar"});
 });
 
+/* Inicialização do servidor */
 app.listen(4000, () => {
   console.log("Aplicação executanto na porta 4000!");
 });
+
+
